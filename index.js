05eeee32269e05a3236cb097262d4bbbc77d7c31
Lisa Chan <xlisachan@gmail.com>
2018-02-20 01:04:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function doToElementsInArray(array, element) {
-  array.doToElementsInArray.forEach(element => {
-    return `Mmmm, ${element}!!!`;
-});
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
